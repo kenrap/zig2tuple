15ef6212de673873c7eb9e5feb39f251c31b8bae
Kenneth Raplee <kenrap@kennethraplee.com>
2025-06-29 09:37:25 -0700

    Refactor `Dependency` handling

--- main.zig
+++ main.zig
@@ -7,36 +7,32 @@ const Dependency = struct {
     name: []const u8,
     url: []const u8,
     hash: []const u8,
-};
+    _indexOffset: usize,
 
-pub fn entryContent(contents: []const u8, entry: []const u8) ?[]const u8 {
-    const index: usize = mem.indexOf(u8, contents, entry) orelse return null;
-    const start: usize = mem.indexOf(u8, contents[index + 1..], "\"") orelse return null;
-    const end: usize = mem.indexOf(u8, contents[index + start + 2..], "\"") orelse return null;
-    return contents[index + start + 2..index + start + end + 2];
-}
+    pub fn init(slice: []const u8) ?Dependency {
+        const dot = mem.indexOf(u8, slice, ".") orelse return null;
+        const afterName = mem.indexOf(u8, slice[dot..], " ") orelse return null;
 
-pub fn findDependency(slice: []const u8) ?struct { usize, Dependency } {
-    const dot = mem.indexOf(u8, slice, ".") orelse return null;
-    const afterName = mem.indexOf(u8, slice[dot..], " ") orelse return null;
+        const open = mem.indexOf(u8, slice[dot + afterName + 1..], "{").?;
+        const start = dot + afterName + open + 1;
+        const close = mem.indexOf(u8, slice[start..], "}").?;
+        const contents = slice[start + 1..start + close];
 
-    const open = mem.indexOf(u8, slice[dot + afterName + 1..], "{").?;
-    const start = dot + afterName + open + 1;
-    const close = mem.indexOf(u8, slice[start..], "}").?;
-    const contents = slice[start + 1..start + close];
-
-    const url = entryContent(contents, ".url");
-    const hash = entryContent(contents, ".hash");
-    
-    return .{
-        start + close,
-        Dependency {
+        return Dependency {
             .name = slice[dot + 1..dot + afterName],
-            .url = url orelse return null,
-            .hash = hash orelse return null,
-        },
-    };
-}
+            .url = entry(contents, ".url") orelse return null,
+            .hash = entry(contents, ".hash") orelse return null,
+            ._indexOffset = start + close,
+        };
+    }
+
+    fn entry(contents: []const u8, key: []const u8) ?[]const u8 {
+        const index: usize = mem.indexOf(u8, contents, key) orelse return null;
+        const start: usize = (mem.indexOf(u8, contents[index + 1..], "\"") orelse return null) + index;
+        const end: usize = (mem.indexOf(u8, contents[start + 2..], "\"") orelse return null) + start;
+        return contents[start + 2..end + 2];
+    }
+};
 
 const DependencyIterator = struct {
     const Self = @This();
@@ -53,12 +49,9 @@ const DependencyIterator = struct {
 
     pub fn next(self: *Self) ?Dependency {
         const slice = self.buffer[self.index + 1..];
-        const dependency = findDependency(slice);
-        if (dependency == null)
-            return null;
-        const result = dependency.?;
-        self.index += result[0];
-        return result[1];
+        const dep = Dependency.init(slice) orelse return null;
+        self.index += dep._indexOffset;
+        return dep;
     }
 };
 
